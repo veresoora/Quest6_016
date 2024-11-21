9d4d5210d94510b20877e1c58fd64e42472c3b6c
veresoora <lahanabira@gmail.com>
2024-11-22 01:24:01 +0700

    membuat button kembali dan lanjut

--- app/src/main/java/com/nadia/pertemuan7/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/nadia/pertemuan7/ui/view/screen/RencanaStudyView.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
@@ -167,7 +168,18 @@ fun RencanaStudyView(
                         fontWeight = FontWeight.Light,
                         fontSize = 10.sp
                         )
-
+                }
+                Spacer(modifier = Modifier.padding(8.dp))
+                Row (
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ){
+                    Button(onClick = {onSubmitButtonClicked(listData)}) {
+                        Text(text = "Kembali")
+                    }
+                    Button(onClick = {onSubmitButtonClicked(listData)}, enabled = checked) {
+                        Text(text = "Lanjut")
+                    }
                 }
             }
         }
