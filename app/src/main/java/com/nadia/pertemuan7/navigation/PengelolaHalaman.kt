57b7203177cbbcda66e1a5459772092c0299bcc1
veresoora <lahanabira@gmail.com>
2024-11-22 01:53:11 +0700

    membuat enum class Halaman

--- app/src/main/java/com/nadia/pertemuan7/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/nadia/pertemuan7/navigation/PengelolaHalaman.kt
@@ -1,2 +1,15 @@
 package com.nadia.pertemuan7.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.nadia.pertemuan7.ui.view.viewmodel.MahasiswaViewModel
+
+enum class Halaman {
+    Splash,
+    Mahasiswa,
+    Matakuliah,
+    Tampil
+}
