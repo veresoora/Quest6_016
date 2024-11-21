c9b8a9c216150d614ddb624e26213a29c958434d
veresoora <lahanabira@gmail.com>
2024-11-22 00:45:21 +0700

    menambahkan icon

--- app/src/main/java/com/nadia/pertemuan7/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/nadia/pertemuan7/ui/view/screen/RencanaStudyView.kt
@@ -11,6 +11,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -78,7 +81,11 @@ fun RencanaStudyView(
                 )
             }
             Box {
-
+                Icon(
+                    imageVector = Icons.Filled.Notifications,
+                    contentDescription = "",
+                    tint = Color.White
+                )
             }
         }
     }
