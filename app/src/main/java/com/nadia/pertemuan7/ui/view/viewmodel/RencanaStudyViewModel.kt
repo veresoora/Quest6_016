fca19ea2ff0ff1e7191018dc418a4eb26f946339
veresoora <lahanabira@gmail.com>
2024-11-22 00:23:23 +0700

    membuat variabel hanya untuk akses internet class

--- app/src/main/java/com/nadia/pertemuan7/ui/view/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/nadia/pertemuan7/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -1,4 +1,10 @@
 package com.nadia.pertemuan7.ui.view.viewmodel
 
+import com.nadia.pertemuan7.model.RencanaStudi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
 class RencanaStudyViewModel {
+    private val _krsState = MutableStateFlow(RencanaStudi())
 }
