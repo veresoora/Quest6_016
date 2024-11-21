dd5ee4350951a7528c56af627cb45934aeae9beb
veresoora <lahanabira@gmail.com>
2024-11-22 01:33:31 +0700

    membuat fungsi DetailMahasiswa

--- app/src/main/java/com/nadia/pertemuan7/ui/view/screen/DetailView.kt
+++ app/src/main/java/com/nadia/pertemuan7/ui/view/screen/DetailView.kt
@@ -1,6 +1,13 @@
 package com.nadia.pertemuan7.ui.view.screen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.nadia.pertemuan7.model.Mahasiswa
 import com.nadia.pertemuan7.model.RencanaStudi
 
@@ -18,4 +25,32 @@ fun DetailView(dataMHS: Mahasiswa,
         Pair("Mata Kuliah", dataKRS.mataKuliah),
         Pair("Kelas", dataKRS.kelas)
     )
+}
+
+@Composable
+fun DetailMahasiswa(
+    judul: String,
+    isinya: String
+){
+    Column(
+        modifier = Modifier.fillMaxWidth().padding(16.dp)
+    ) {
+        Row (
+            modifier = Modifier.fillMaxWidth()
+                .padding(top = 2.dp)
+        ){
+            Text(
+                text = judul,
+                modifier = Modifier.weight(1f)
+            )
+            Text(
+                text = ".",
+                modifier = Modifier.weight(0.2f)
+            )
+            Text(
+                text = isinya,
+                modifier = Modifier.weight(2f)
+            )
+        }
+    }
 }
