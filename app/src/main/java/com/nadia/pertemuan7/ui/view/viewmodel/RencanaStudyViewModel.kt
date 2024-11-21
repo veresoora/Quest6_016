4fdd2d04b52ac6983f693309ff49dc8bdf0b5b14
veresoora <lahanabira@gmail.com>
2024-11-22 00:27:25 +0700

    membuat fungsi setMataKuliah

--- app/src/main/java/com/nadia/pertemuan7/ui/view/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/nadia/pertemuan7/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -4,8 +4,13 @@ import com.nadia.pertemuan7.model.RencanaStudi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class RencanaStudyViewModel {
     private val _krsState = MutableStateFlow(RencanaStudi())
     val krsStateUi : StateFlow<RencanaStudi> = _krsState.asStateFlow()
+
+    fun setMataKuliah(mkPilihan: String){
+        _krsState.update { stateMk -> stateMk.copy(mataKuliah = mkPilihan) }
+    }
 }
