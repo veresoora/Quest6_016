6e55a7402f32eb1b763fa937184c8605e9697c22
veresoora <lahanabira@gmail.com>
2024-11-22 00:41:20 +0700

    membuat variabel agar dapat diakses secara publik

--- app/src/main/java/com/nadia/pertemuan7/ui/view/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/nadia/pertemuan7/ui/view/viewmodel/MahasiswaViewModel.kt
@@ -3,7 +3,10 @@ package com.nadia.pertemuan7.ui.view.viewmodel
 import androidx.lifecycle.ViewModel
 import com.nadia.pertemuan7.model.Mahasiswa
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class MahasiswaViewModel : ViewModel() {
     private val _dataModel = MutableStateFlow(Mahasiswa())
+    val dataModel: StateFlow<Mahasiswa> = _dataModel.asStateFlow()
 }
