173b91b9e9e3f0fab28dd87c75b86adedb650c4a
veresoora <lahanabira@gmail.com>
2024-11-21 23:30:19 +0700

    menambahkan data di object MataKuliah

--- app/src/main/java/com/nadia/pertemuan7/data/MataKuliah.kt
+++ app/src/main/java/com/nadia/pertemuan7/data/MataKuliah.kt
@@ -1,4 +1,12 @@
 package com.nadia.pertemuan7.data
 
 object MataKuliah {
+    val option = listOf(
+        "Pemograman Aplikasi Mobile Lanjut",
+        "Pengembangan Aplikasi Web Framework",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer Vission"
+    )
 }
