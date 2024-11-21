ea64a214cc9b8558080768f8e08aa8429cdf3dee
veresoora <lahanabira@gmail.com>
2024-11-22 01:27:24 +0700

    membuat fungsi untuk DetailView

---
+++ app/src/main/java/com/nadia/pertemuan7/ui/view/screen/DetailView.kt
@@ -0,0 +1,13 @@
+package com.nadia.pertemuan7.ui.view.screen
+
+import androidx.compose.runtime.Composable
+import com.nadia.pertemuan7.model.Mahasiswa
+import com.nadia.pertemuan7.model.RencanaStudi
+
+@Composable
+fun DetailView(dataMHS: Mahasiswa,
+               dataKRS: RencanaStudi,
+               onBackButtonClicked: () -> Unit
+){
+
+}
