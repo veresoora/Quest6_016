e96808bdc879381bef7cb996399478c5f00c10bb
veresoora <lahanabira@gmail.com>
2024-11-22 01:36:36 +0700

    membuat tampilan detail data

--- app/src/main/java/com/nadia/pertemuan7/ui/view/screen/DetailView.kt
+++ app/src/main/java/com/nadia/pertemuan7/ui/view/screen/DetailView.kt
@@ -2,8 +2,10 @@ package com.nadia.pertemuan7.ui.view.screen
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -25,6 +27,34 @@ fun DetailView(dataMHS: Mahasiswa,
         Pair("Mata Kuliah", dataKRS.mataKuliah),
         Pair("Kelas", dataKRS.kelas)
     )
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize()
+    ) {
+        listDataMHS.forEach { data ->
+            DetailMahasiswa(
+                judul = data.first,
+                isinya = data.second
+            )
+        }
+
+        listDataKRS.forEach { data ->
+            DetailMahasiswa(
+                judul = data.first,
+                isinya = data.second
+            )
+        }
+
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = {
+                onBackButtonClicked()
+            }
+        ) {
+            Text(text = "Kembali")
+        }
+    }
 }
 
 @Composable
