38c8d7031750a470871e34cdd36fa3dc4e573756
veresoora <lahanabira@gmail.com>
2024-11-22 00:39:58 +0700

    Membuat variabel hanya untuk akses internet class

--- app/src/main/java/com/nadia/pertemuan7/ui/view/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/nadia/pertemuan7/ui/view/viewmodel/MahasiswaViewModel.kt
@@ -1,6 +1,9 @@
 package com.nadia.pertemuan7.ui.view.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.nadia.pertemuan7.model.Mahasiswa
+import kotlinx.coroutines.flow.MutableStateFlow
 
 class MahasiswaViewModel : ViewModel() {
+    private val _dataModel = MutableStateFlow(Mahasiswa())
 }
