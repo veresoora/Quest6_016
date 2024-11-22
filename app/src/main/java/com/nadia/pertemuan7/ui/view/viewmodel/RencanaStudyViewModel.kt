0be161b326e0e7eccbec0a364a75530581cd451d
veresoora <lahanabira@gmail.com>
2024-11-22 16:41:41 +0700

    menambahkan ViewModel() yang terlewat

--- app/src/main/java/com/nadia/pertemuan7/ui/view/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/nadia/pertemuan7/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -1,12 +1,13 @@
 package com.nadia.pertemuan7.ui.view.viewmodel
 
+import androidx.lifecycle.ViewModel
 import com.nadia.pertemuan7.model.RencanaStudi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
-class RencanaStudyViewModel {
+class RencanaStudyViewModel : ViewModel() {
     private val _krsState = MutableStateFlow(RencanaStudi())
     val krsStateUi : StateFlow<RencanaStudi> = _krsState.asStateFlow()
 
